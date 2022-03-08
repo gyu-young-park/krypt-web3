fd2125501a710892fb953ea0a12165df84040cc6
gyu-young-park <gyoue200125@gmail.com>
2022-03-08 23:14:49 +0900

    chore: set up smart-contract environment

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -15,7 +15,6 @@ const Input = ({ placeholder, name, type, value, handleChange}) => (
         onChange={(e)=> handleChange(e, name)}
         className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
     />
-
 )
 
 const Welcome = () => {
